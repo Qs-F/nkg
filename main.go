a868b2834f16452bb4f6493f10a6cf2fdfb94645
Qs-F <qsf@de-liker.com>
2020-04-01 03:48:32 +0900

    Fix to trim package.json from returned value

--- main.go
+++ main.go
@@ -4,19 +4,22 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/Qs-F/walkup"
 )
+
+const target = "package.json"
 
 func main() {
 	wd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-	ps := walkup.Walkup(wd, "package.json", 0)
+	ps := walkup.Walkup(wd, target, 0)
 	if len(ps) < 1 {
 		fmt.Println(".")
 		os.Exit(1)
 	}
-	fmt.Println(ps[0])
+	fmt.Println(filepath.Dir(ps[0]))
 }
