937aea385f65440e7cd215a8b438d436842627e9
Qs-F <qsf@de-liker.com>
2020-04-01 03:33:54 +0900

    Add main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Qs-F/walkup"
+)
+
+func main() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	ps := walkup.Walkup(wd, "package.json", 0)
+	if len(ps) < 1 {
+		os.Exit(1)
+	}
+	fmt.Println(ps[0])
+}
